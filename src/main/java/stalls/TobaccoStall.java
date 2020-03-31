492ee8c725782eedf0ce7070c12ac9b339944b4d
JohnHG1 <honisz@yahoo.com>
2020-03-31 18:52:56 +0100

    Initial commit MVP completed

---
+++ src/main/java/stalls/TobaccoStall.java
@@ -0,0 +1,24 @@
+package stalls;
+
+import behaviours.IReviewed;
+import behaviours.ISecurity;
+import people.Visitor;
+
+public class TobaccoStall extends Stall implements ISecurity, IReviewed {
+
+    public TobaccoStall(String name, String ownerName, ParkingSpot parkingSpot) {
+        super(name, ownerName, parkingSpot);
+    }
+
+
+    @Override
+    public boolean isAllowedTo(Visitor visitor) {
+        return visitor.getAge() > 17;
+    }
+
+
+    @Override
+    public int getRating() {
+        return 0;
+    }
+}
